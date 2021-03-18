dd497dbd66fd833eab6a393305f70e64bf78ec41
Alex <adchengzhi7@gmail.com>
2021-03-18 18:01:32 +0800

    init setting

---
+++ backend/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mysql = require('mysql');
+const gets = require('./db');
+
+ 
+const router = express.Router();
+
+gets.gets()
+//  
+
+/* mysql連線測試 */
+
+module.exports = router;
+
+/* GET users listing. */
+
+
+// module.exports = router;
