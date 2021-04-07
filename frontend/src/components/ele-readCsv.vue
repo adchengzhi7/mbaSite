da4a70fad795fd886c2abbe8e74e1ca1d171dc19
Alex <adchengzhi7@gmail.com>
2021-04-08 00:32:47 +0800

    FRONTEND: change package --> import csv get data to vuex and render on table

--- frontend/src/components/ele-readCsv.vue
+++ frontend/src/components/ele-readCsv.vue
@@ -1,94 +1,67 @@
 <template>
     <div class="p-5">
-   <vue-csv-import 
-    v-model="csv"
-    :fields="{
-        name: {required: true, label: 'Name'}, personalid: {required: true, label: 'personalid'},
-        studentid: {required: true, label: 'studentid'}, email: {required: true, label: 'email'}
-    }"
-    :text="text"
-    v-slot="{errors}"
-    >   
-    
-  
-
-    <label for="file-upload" class="custom-file-upload">
-        <i class="fa fa-cloud-upload"></i>上傳CSV
-    </label>
-    <div class="text-danger font-weight-boldest" :key="index" v-for="(error,index) in errors">
-    {{error}}
-    </div>
-    <vue-csv-input id="file-upload"></vue-csv-input>
-
-        <vue-csv-toggle-headers v-slot="{hasHeaders, toggle}">
-            <div>
-            <button v-if="hasHeaders" @click.prevent="toggle">包含標題</button>
-            <button v-else @click.prevent="toggle">不包含標題</button>
-            </div>
-        </vue-csv-toggle-headers>
-        
-    <vue-csv-map  :auto-match="true" v-slot="{sample, map}">
-        <div class="d-none">
-            <div>{{sample}}</div>
-                 <div>{{map}}</div>
+        <input type="file" @change="test"  accept=".csv">
+        <div class="text-muted"> 共 {{countCsv}} 比資料
         </div>
-        
-    </vue-csv-map>
-    
-</vue-csv-import>
-    {{countCsv}}
+        <div v-if="iserror" class="text-danger">
+            {{error}}
+        </div>
     </div>
 </template>
 
 <script>
 
-import {VueCsvToggleHeaders, VueCsvSubmit, VueCsvMap, VueCsvInput, VueCsvErrors, VueCsvImport} from 'vue-csv-import';
 import {mapActions} from 'vuex'
-
+import {csv} from "csvtojson";  
 
 export default {
+    
     component:{
-        VueCsvToggleHeaders, VueCsvSubmit, VueCsvMap, 
-        VueCsvInput, VueCsvErrors, VueCsvImport,
     },
     data() {
     return {
-        text:{
-            "errors": {
-                "fileRequired" : "至少選擇一個檔案",
-                "invalidMimeType" : "檔案類型錯誤❌"
-            },
-            "toggleHeaders": "File has headers",
-            "submitBtn": "Submit",
-            "fieldColumn": "Field",
-            "csvColumn": "Column"
-        },
-        csv:[],
-        file:'',
-
+        jsonData:[],
+        iserror:false,
+        error:"檔案資料格式錯誤",
     }
 },
 computed:{
     countCsv(){
-        let length =this.csv.length;
-        let vm = this;
+        let vm =this;
+        let length =vm.jsonData.length;
         if(length >0){
-            vm.newStudent(vm.csv)
-            return  this.csv.length
-        }
-        else return 0
+            vm.newStudent(vm.jsonData)
+            return  length
+        }else{
+           
+            return 0
+        } 
+            
     }
 },
 methods: {
 ...mapActions({
     newStudent:'student/newStudent'
-})
-   
+}),
+    async test(input){
+        let vm =this;
+        if(input.target.files && input.target.files[0]){
+        let tmppath = URL.createObjectURL(input.target.files[0]);
+        const res = await fetch(tmppath);
+        const text = await res.text();
+        const jsonArray = await csv().fromString(text);
+        vm.jsonData = jsonArray;
+        if(jsonArray.length === 0){
+             vm.iserror= true;
+        }
+
+        }
+    },
 },
 }
 </script>
 <style scoped>
-input[type="file"] {
+/* input[type="file"] {
     position: fixed;
     right: 100%;
     bottom: 100%;
@@ -98,5 +71,5 @@ input[type="file"] {
     display: inline-block;
     padding: 60px 120px;
     cursor: pointer;
-}
+} */
 </style>
